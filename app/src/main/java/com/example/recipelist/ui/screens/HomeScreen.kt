c896eef5c11f53f2ccbb3992de7fa252ac337e1a
xofiaxinha <msofia.lima@alu.ufc.br>
2025-05-30 21:35:20 -0300

    HomeScreen

--- app/src/main/java/com/example/recipelist/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/recipelist/ui/screens/HomeScreen.kt
@@ -9,17 +9,21 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,11 +47,6 @@ fun HomeScreen(recipes: List<Recipe>){
                     text = "Recipe Cart",
                     style = MaterialTheme.typography.titleLarge
                 ) },
-                //floatingActionButton = {
-                  //  FloatingActionButton(onClick = {}) {
-                    //    Icon(Icons.Filled.Add, contentDescription = "Adicionar ingredientes à lista")
-                   // }
-                //}
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Color(0xD64D50ff),
                     titleContentColor = Color.White,

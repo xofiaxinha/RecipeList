339ec050349b1d20d7b8ec08d8e4e533b750c3d3
xofiaxinha <msofia.lima@alu.ufc.br>
2025-07-22 15:00:11 -0300

    login + ui login

--- app/src/main/java/com/example/recipelist/ui/theme/Color.kt
+++ app/src/main/java/com/example/recipelist/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.example.recipelist.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val MainRed = Color(0xFFE63244) // cor de destaque
+val DarkMainRed = Color(0xFF85141B)
 val RedGray80 = Color(0xFFBBADAD) // cinza claro
 val RedGray70 = Color(0xFFB2A0A0) // cinza claro
 val RedGrayT = Color(0x44BBADAD) // cor transparente
